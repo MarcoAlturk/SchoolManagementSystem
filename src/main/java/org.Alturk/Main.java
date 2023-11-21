51264613918146613038c7490cb2ed2f0eb66d30
MarcoAlturk <2359284@edu.vaniercollege.qc.ca>
2023-11-21 12:25:34 -0500

    Add Constructors

--- src/main/java/org.Alturk/Main.java
+++ src/main/java/org.Alturk/Main.java
@@ -1,8 +1,14 @@
 package org.Alturk;
 
+
+import org.Alturk.dto.Department;
+
 /**
  * @author Marco Alturk (2359284)
  */
 public class Main {
-
+    public static void main(String[] args) {
+        Department department = new Department("Caca");
+        System.out.println(department);
+    }
 }
